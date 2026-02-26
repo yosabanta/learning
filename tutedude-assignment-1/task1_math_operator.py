a98b7dd4a0ea88e1f3370d7ac00f13614b4e57f8
yosabanta <yosabantabarik.06@gmail.com>
2026-02-26 12:13:43 +0530

    Add task 1 and Task 2 solutions

---
+++ tutedude-assignment-1/task1_math_operator.py
@@ -0,0 +1,9 @@
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("Addition:",a+b)
+print("Subtraction:",a-b)
+print("Multiplication:",a*b)
+if b != 0:
+    print("Division:",a/b)
+else:
+    print("Division cannot be performed by zero")
---
+++ tutedude-assignment-1/task2_greeting.py
@@ -0,0 +1,6 @@
+first_name = str(input("Enter your first name: "))
+last_name = str(input("Enter your last name: "))
+
+full_name = first_name + " " + last_name
+
+print(f'Hello,{full_name}! Welcome to the python program.')
